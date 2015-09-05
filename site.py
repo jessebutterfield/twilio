c8cc9ab57ee359c240a3280e36be6289a5cea031
Jesse Butterfield <jbutterfield@quantcast.com>
2015-09-05 14:29:02 -0700

    Initial functionality

--- site.py
+++ site.py
@@ -1,14 +1,54 @@
 #!/usr/local/bin/python3.4
 import cherrypy
+from twilio.util import TwilioCapability
+import twilio.twiml
+
+# Account Sid and Auth Token can be found in your account dashboard
+ACCOUNT_SID = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+AUTH_TOKEN = 'YYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY'
+
+# TwiML app outgoing connections will use
+APP_SID = 'APZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ'
+
+CALLER_ID = '+12345678901'
+CLIENT = 'jenny'
 
 class Root(object):
+
+    @cherrypy.expose
+    def token():
+      account_sid = os.environ.get("ACCOUNT_SID", ACCOUNT_SID)
+      auth_token = os.environ.get("AUTH_TOKEN", AUTH_TOKEN)
+      app_sid = os.environ.get("APP_SID", APP_SID)
+
+      capability = TwilioCapability(account_sid, auth_token)
+
+      # This allows outgoing connections to TwiML application
+      if request.values.get('allowOutgoing') != 'false':
+         capability.allow_client_outgoing(app_sid)
+
+      # This allows incoming connections to client (if specified)
+      client = request.values.get('client')
+      if client != None:
+        capability.allow_client_incoming(client)
+
+      # This returns a token to use with Twilio based on the account and capabilities defined above
+      return capability.generate()
+
+    @cherrypy.expose
+    def welcome():
+        resp = twilio.twiml.Response()
+        resp.say("Welcome to Twilio")
+        return str(resp)
+
+
     @cherrypy.expose
     def index(self, **args):
-            return '<Response><Sms>Stop masturbating in the shower. There is a drought.</Sms></Response>'
+        return '<Response><Sms>Stop masturbating in the shower. There is a drought.</Sms></Response>'
 
     @cherrypy.expose
     def reply(self, **args):
-            return '<Response><Sms>Something less work inappropriate.</Sms></Response>'
+        return '<Response><Sms>Something less work inappropriate.</Sms></Response>'
 
 cherrypy.config.update({
     'environment': 'production',
